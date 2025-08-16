05b90375d4e00f5c0d932871ec9b1975fdba24e7
unknown <chrystabel.clemencie.ang@gmail.com>
2025-08-16 11:27:17 +0700

    update

--- SVM Data Training.py
+++ SVM Data Training.py
@@ -8,7 +8,7 @@ from sklearn.metrics import classification_report, confusion_matrix
 import joblib
 
 # 1. Load dataset EEG
-df = pd.read_csv('eeg_raw_dataset.csv')
+df = pd.read_csv('../FINAL DATASETS/frontal_raw_data.csv')
 print(df.shape)
 
 # 2. Pisahkan fitur dan label
@@ -18,7 +18,7 @@ y = df['label'].values
 print(X.shape)
 
 # 3. Bagi dataset untuk training dan testing
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
 # 4. Buat pipeline: normalisasi + SVM
 pipeline = make_pipeline(
@@ -27,15 +27,15 @@ pipeline = make_pipeline(
 )
 
 # 5. Latih model
-pipeline.fit(X, y)
+pipeline.fit(X_train, y_train)
 
 # 6. Evaluasi model
-y_pred = pipeline.predict(X)
+y_pred = pipeline.predict(X_test)
 print("=== Classification Report ===")
-print(classification_report(y, y_pred))
+print(classification_report(y_test, y_pred))
 print("\n=== Confusion Matrix ===")
-print(confusion_matrix(y, y_pred))
+print(confusion_matrix(y_test, y_pred))
 
 # 7. Simpan model
-joblib.dump(pipeline, 'svm_eeg_model.pkl')
-print("Model saved as 'svm_eeg_model.pkl'")
+joblib.dump(pipeline, '../FINAL DATASETS/frontal_svm_eeg_model.pkl')
+print("Model saved as 'frontal_svm_eeg_model.pkl'")
